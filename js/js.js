218ee7249b1b42e5e3b15f5a016b988d2d3a4f63
0Hasan0 <hr145310@gmail.com>
2023-07-26 00:56:25 +0300

    responsive with phones
    1)make it responsive with phones.
    2)cards toggle.
    3)image input use drop or select.
     ### GOOD WORK TO ME ####

--- js/js.js
+++ js/js.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     item.innerHTML += svgData;
                     item.innerHTML += svgData;
                     item.innerHTML += svgData;
-                    item.innerHTML += "SCAN CODE TO ADD";
+                    item.innerHTML += "<span>SCAN CODE TO ADD</span>";
                     item.innerHTML += svgData;
                     item.innerHTML += svgData;
                     item.innerHTML += svgData;
@@ -49,4 +49,99 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     loadSVG();
+});
+
+// Drop
+$(document).ready(function () {
+    const dropZone = $('#dropZone');
+    const previewImage = $('#previewImage');
+
+    // Prevent default behavior to enable dropping files
+    dropZone.on('dragover', function (e) {
+        e.preventDefault();
+        dropZone.addClass('drag-over');
+    });
+
+    dropZone.on('dragleave', function () {
+        dropZone.removeClass('drag-over');
+    });
+
+    dropZone.on('drop', function (e) {
+        e.preventDefault();
+        dropZone.removeClass('drag-over');
+
+        const files = e.originalEvent.dataTransfer.files;
+        if (files.length > 0) {
+            const file = files[0];
+            const fileType = file.type.split('/')[0];
+            if (fileType === 'image') {
+                // Display the dropped image
+                const reader = new FileReader();
+                reader.onload = function () {
+                    previewImage.attr('src', reader.result);
+                    previewImage.css({
+                        display: 'block'
+                    });
+                    dropZone.find('span').hide();
+                    dropZone.find('svg').hide();
+
+                    // Reset the image size
+                    previewImage.css({
+                        width: '30%',
+                    });
+                };
+                reader.readAsDataURL(file);
+            } else {
+                alert('Please drop an image file (PNG, JPEG, etc.)');
+            }
+        }
+    });
+
+    // When the drop zone is clicked, trigger the click event on the hidden file input
+    dropZone.on('click', function () {
+        const fileInput = $('<input type="file" style="display: none;">');
+        fileInput.on('change', function (e) {
+            const file = e.target.files[0];
+            const fileType = file.type.split('/')[0];
+            if (fileType === 'image') {
+                const reader = new FileReader();
+                reader.onload = function () {
+                    previewImage.attr('src', reader.result);
+                    previewImage.css({
+                        display: 'block'
+                    });
+                    dropZone.find('span').hide();
+                    dropZone.find('svg').hide();
+
+                    // Reset the image size
+                    previewImage.css({
+                        width: '30%',
+                    });
+                };
+                reader.readAsDataURL(file);
+            } else {
+                alert('Please select an image file (PNG, JPEG, etc.)');
+            }
+        });
+        fileInput.trigger('click');
+    });
+});
+
+// toggle btns
+$(document).ready(function () {
+    $("#toggle-card-1").on("click", function () {
+        $("#toggle-card-2").removeClass("active");
+        $(this).toggleClass("active");
+        $(".offline").appendTo(".content");
+        $(".offline").css("display", "flex");
+        $(".online").hide();
+    });
+
+    $("#toggle-card-2").on("click", function () {
+        $("#toggle-card-1").removeClass("active");
+        $(this).toggleClass("active");
+        $(".online").appendTo(".content");
+        $(".online").css("display", "flex");
+        $(".offline").hide();
+    });
 });
