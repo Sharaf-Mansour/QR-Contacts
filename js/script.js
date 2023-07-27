69c72abdd62f39db52fcb1f22b150492288739c1
Sharaf M. Mansour <sharaf_1015@yahoo.com>
2023-07-27 04:25:09 +0300

    Add map location loader

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var url = window.location.href;
-var input = LZString.decompressFromEncodedURIComponent(url.slice(url.indexOf("?") + 1));
+var input = LZString.decompressFromEncodedURIComponent(url.slice(url.indexOf("?") + 1)); 
+
 var url = url.slice(0, url.indexOf("?") + 1) + input;
 let data = url.slice(url.indexOf('?') + 1).split('~');
 
@@ -13,6 +14,17 @@ let locationinfo = data[7];
 let faceBook = data[8];
 let linkedIn = data[9];
 let telegram = data[10];
+let latitude = data[11];
+let longitude = data[12];
+let photo = data[13];
+ 
+
+ 
+    const map = L.map('mapid').setView([latitude, longitude], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+         maxZoom: 18,
+    }).addTo(map);
+    L.marker([latitude, longitude]).addTo(map);
 
 
 // use jqery to set the value of the span tags
@@ -35,6 +47,10 @@ $(document).ready(function () {
     $("#company").text(company);
     $("#workinfo").text(workinfo);
     $("#locationinfo").text(locationinfo);
-
+    $("#photo").attr("src", photo);
 }
+
+
+
 );
+
